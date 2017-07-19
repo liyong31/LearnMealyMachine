f7fd9a93068273358cef5008e187df760c286e56
findsat <duckly_lee@163.com>
2017-07-19 16:40:50 +0800

    random test for dfa learning

--- src/test/java/cn/ac/ios/dfa/DFARandomTest.java
+++ src/test/java/cn/ac/ios/dfa/DFARandomTest.java
@@ -1,16 +1,27 @@
+/* Copyright (c) 2016, 2017                                               */
+/*       Institute of Software, Chinese Academy of Sciences               */
+/* This file is part of ROLL, a Regular Omega Language Learning library.  */
+/* ROLL is free software: you can redistribute it and/or modify           */
+/* it under the terms of the GNU General Public License as published by   */
+/* the Free Software Foundation, either version 3 of the License, or      */
+/* (at your option) any later version.                                    */
+
+/* This program is distributed in the hope that it will be useful,        */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of         */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          */
+/* GNU General Public License for more details.                           */
+
+/* You should have received a copy of the GNU General Public License      */
+/* along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
 package cn.ac.ios.dfa;
 
 import java.util.Random;
-import java.util.TreeMap;
 
 import cn.ac.ios.learner.table.dfa.LearnerDFATable;
 import cn.ac.ios.machine.Machine;
 import cn.ac.ios.machine.State;
 import cn.ac.ios.machine.dfa.DFA;
-import cn.ac.ios.mealy.EquivalenceOracleImpl;
-import cn.ac.ios.mealy.InputHelper;
-import cn.ac.ios.oracle.EquivalenceOracle;
-import cn.ac.ios.oracle.MembershipOracle;
 import cn.ac.ios.query.Query;
 import cn.ac.ios.table.HashableValue;
 import cn.ac.ios.words.Alphabet;
